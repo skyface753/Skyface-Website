b6f16633c979ca2312b024c464baf146554d9e6d
Sebastian Jörz <sjoerz@skyface.de>
2022-05-21 01:12:07 +0200

    Aboutpage from JSON

--- skyface-web/src/pages/about.js
+++ skyface-web/src/pages/about.js
@@ -1,40 +1,43 @@
-import React from 'react';
+import React from "react";
+import aboutData from "../page-content/about-page.json";
 
 const About = () => {
-    return (
-        <div
-            // style={
-            //     {
-            //         width: '80%',
-            //         margin: '0 auto',
-            //         textAlign: 'center',
-            //     }
-            // }
-            >
-            <div className='title-container'>
-                <img src={require('../img/about-title.png')}
-                    width="100%" alt='About-Title' />
-                <div className='title-container-text'>
-                    <h1>Welcome to SkyBlog</h1>
-                    <h2>Coding-Blog</h2>
-                </div>
-            </div>
-            {/* Divider */}
-            <div className="divider" />
-            {/* Card */}
-            <div >
-                <div >
-                    <h5>About</h5>
-                    <p>This is a blog for coding related stuff.</p>
-                </div>
-                <div>Column 1</div>
-                <div>Column 2</div>
-                <div>Column 3</div>
-                <div>Column 4</div>
-                
-            </div>
+  return (
+    <div
+    // style={
+    //     {
+    //         width: '80%',
+    //         margin: '0 auto',
+    //         textAlign: 'center',
+    //     }
+    // }
+    >
+      <div className="title-container">
+        <img
+          src={require("../img/about-title.png")}
+          width="100%"
+          alt="About-Title"
+        />
+        <div className="title-container-text">
+          <h1>{aboutData.header.title}</h1>
+          <h2>{aboutData.header.subtitle}</h2>
         </div>
-    );
+      </div>
+      {/* Divider */}
+      <div className="divider" />
+
+      {/* About */}
+      {(() => {
+        var about = aboutData.content;
+        var aboutDivs = [];
+        console.log(about);
+        for (var i = 0; i < about.length; i++) {
+          aboutDivs.push(<p>{about[i].text}</p>);
+        }
+        return aboutDivs;
+      })()}
+    </div>
+  );
 };
 
 export default About;
