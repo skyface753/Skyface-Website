2bb355706e6a42d5f3eb04ac04dd14ea51be994b
Sebastian Jörz <sjoerz@skyface.de>
2022-12-26 19:56:06 +0100

    del react native

--- skyface_react_native/constants.js
+++
@@ -1,5 +0,0 @@
-const HOSTURL = "https://www.skyface.de/";
-export const BACKENDURL = HOSTURL;
-export const BACKEND_FILES_URL = HOSTURL + "uploaded-files/";
-// export const BACKENDURL = "http://127.0.0.1:5000/";
-// export const BACKEND_FILES_URL = "http://localhost:5000/uploaded-files/";
--- skyface_react_native/services.js/CheckLogin.js
+++
@@ -1,9 +0,0 @@
-import { reactLocalStorage } from "reactjs-localstorage";
-
-export default function CheckLogin(){
-    const loggedInUser = reactLocalStorage.getObject("loggedInUser");
-    if(loggedInUser){
-        return true;
-    }
-    return false;
-}
