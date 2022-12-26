31890d6d2064dd6195603133710a02787a1bd233
Sebastian Jörz <sjoerz@skyface.de>
2022-12-26 19:53:35 +0100

    .

--- skyface-web/src/pages/impressum.js
+++ skyface-web/src/pages/impressum.js
@@ -2,21 +2,14 @@ export default function Impressum() {
   return (
     <div>
       <h1>Impressum</h1>
-      <div class="blog-detail">
-        <h2 id="angaben-gemass-5-tmg">Angaben gemäß § 5 TMG</h2>
+      <div class='blog-detail'>
+        <h2 id='angaben-gemass-5-tmg'>Angaben gemäß § 5 TMG</h2>
         <p>
           Sebastian Joerz
           <br />
-          Im Bangert 65
-          <br />
-          64750 Lützelbach
         </p>
-        <h2 id="kontakt">Kontakt</h2>
-        <p>
-          Telefon: +49 (0)176-41312346
-          <br />
-          E-Mail: seppel8546@gmail.com
-        </p>
+        <h2 id='kontakt'>Kontakt</h2>
+        <p>E-Mail: seppel8546@gmail.com</p>
       </div>
     </div>
   );
